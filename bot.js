fa8250e3fb6488bddfbb5f83f1e38dfc1d036de0
Gabe Labadie <gosaintsgaming@gmail.com>
2019-03-18 01:27:13 -0500

    Removed duplication in bot.js and general cleanup

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-process.on("unhandledRejection", (reason) => {
-  console.log("Reason: " + reason);
-});
 const Discord = require("discord.js");
 const Commando = require("discord.js-commando");
 const client = new Commando.Client({
@@ -48,7 +45,7 @@ const sqlite = require("sqlite");
 
 client.setProvider(
   sqlite.open(path.join(__dirname, "settings.sqlite3")).then(db => new Commando.SQLiteProvider(db))
-).catch(console.error);
+);
 
 global.charactersJson = readJson("./characters.json");
 global.currentCharacter = -1;
@@ -56,9 +53,6 @@ global.rare = false;
 global.currentBoxCode = "";
 global.currentBoxKeyColor = "";
 global.currentBoxMessage = "";
-global.getRaritySymbol = function (rarity) {
-  return rarityOdds.find(f => f.min <= rarity && f.max >= rarity);
-}
 
 global.rarityOdds = {
   "❀": {
@@ -85,7 +79,7 @@ global.rarityOdds = {
     min: 200,
     max: 200
   }
-}
+};
 
 global.rarityTexts = {
   "❀": "alpha",
@@ -94,7 +88,7 @@ global.rarityTexts = {
   "✧": "delta",
   "☆": "epsilon",
   "♛": "zeta"
-}
+};
 
 global.raritySymbols = {
   "alpha": "❀",
@@ -103,6 +97,43 @@ global.raritySymbols = {
   "delta": "✧",
   "epsilon": "☆",
   "zeta": "♛"
+};
+
+global.getRaritySymbol = function (rarity) {
+  return rarityOdds.find((f) => f.min <= rarity && f.max >= rarity);
+}
+
+function checkForNewMembers(member) {
+  var creditsJson = readJson("./credits.json");
+  var currentUser = -1;
+
+  for (var i = 0; i < creditsJson.length; i++) {
+    if (creditsJson[i].id == member.user.id) {
+      currentUser = member.user.id;
+    }
+  }
+
+  if (currentUser == -1) {
+    creditsJson.push({
+      "id": member.user.id,
+      "credits": 0,
+      "nextDaily": 0,
+      "characters": [
+
+      ],
+      "wishlist": [
+
+      ],
+      "favoriteCharacter": -1,
+      "lastGacha": 0,
+      "currentTrade": -1,
+      "keys": [
+
+      ],
+      "lastDailyKeys": 0
+    });
+  }
+  fs.writeFileSync(require.resolve("./credits.json"), JSON.stringify(creditsJson));
 }
 
 client.on("commandError", (command, err, message, args, fromPattern) => {
@@ -117,15 +148,17 @@ client.on("ready", () => {
   var creditsJson = readJson("./credits.json");
   var charactersJson = readJson("./characters.json");
   client.user.setPresence({
-    game: { name: charactersJson.length + " characters" },
+    game: {
+      name: charactersJson.length + " characters"
+    },
     status: "streaming"
   });
 
-  setInterval(function() {
+  setInterval(function () {
     creditsJson = readJson("./credits.json");
     fs.writeFileSync("./credits-backup-" + moment.tz("America/Chicago").format("YYYY-MM-DD-hh-mm-a") + ".json", JSON.stringify(creditsJson));
     client.users.get("247805204487995392").send("Backup saved to" + "./credits-backup-" + moment.tz("America/Chicago").format("YYYY-MM-DD-hh-mm-a") + ".json");
-  },  3600000*24);
+  }, 3600000 * 24);
 
   for (var i = 0; i < client.guilds.array().length; i++) {
     client.guilds.get(client.guilds.array()[i].id).fetchMembers().then(r => {
@@ -133,60 +166,12 @@ client.on("ready", () => {
         if (r.members.array()[j].user.bot == true) {
           continue;
         }
-        var currentUser = -1;
-        for (var k = 0; k < creditsJson.length; k++) {
-          if (creditsJson[k].id == r.members.array()[j].user.id) {
-            currentUser = r.members.array()[j].user;
-          }
-        }
-        if (currentUser == -1) {
-          creditsJson.push({
-            "id": r.members.array()[j].user.id,
-            "credits": 0,
-            "nextDaily": 0,
-            "characters": [
-
-            ],
-            "wishlist": [
-
-            ],
-            "favoriteCharacter": -1,
-            "lastGacha": 0,
-            "currentTrade": -1,
-            "keys": [
-
-            ],
-            "lastDailyKeys": 0
-          });
-          console.log("Added " + r.members.array()[j].user.username);
-          fs.writeFileSync(require.resolve("./credits.json"), JSON.stringify(creditsJson));
-        }
+        checkForNewMembers(r.members.array()[j]);
       }
     });
   }
 
   var messagesJson = readJson("./messages.json");
-
-  /*for (var i = 0; i < client.guilds.array()[0].channels.array().length; i++) {
-    for (var j = 0; j < channelsToTake.length; j++) {
-      if (channelsToTake[j] == parseInt(client.guilds.array()[0].channels.array()[i].id) && client.guilds.array()[0].channels.array()[i].type == "text") {
-        console.log(i);
-        client.guilds.array()[0].channels.array()[i].fetchMessages({ limit: 100 }).then(function(messages) {
-          for(var k = 0; k < messages.array().length; j++) {
-            messagesJson.push({
-              //channel: client.guilds.array()[0].channels.array()[i].name,
-              author: messages.array()[k].author.username,
-              content: messages.array()[k].content
-            });
-          }
-        });
-      }
-    }
-  }
-
-  fs.writeFileSync(require.resolve("./messages.json"), JSON.stringify(messagesJson));*/
-
-  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", async msg => {
@@ -195,57 +180,38 @@ client.on("message", async msg => {
   var random = Math.floor(Math.random() * 100);
   var rareRandom = Math.floor(Math.random() * 250);
   var boxRandom = Math.floor(Math.random() * 100);
-  
-  //console.log("Random: " + random);
+  var title;
+  var spawnableCharacters;
 
   if (!msg.author.bot && random == 1) {
-    if (rareRandom  == 1) {
+    if (rareRandom == 1) {
       rare = true;
-      var rareCharacters = charactersJson.filter(f => f.rare);
-      var ran = Math.floor(Math.random() * rareCharacters.length);
-
-      currentCharacter = charactersJson.findIndex(f => f.name.toLowerCase() == rareCharacters[ran].name.toLowerCase());
-      var desc = "A character appeared!\nTry guessing their name with ``=claim <name>`` to claim them!\n\nHints:\nThis character's initials are ";
-      for (var i = 0; i < charactersJson[currentCharacter].name.split(" ").length; i++) {
-        desc += charactersJson[currentCharacter].name.split(" ")[i].substring(0, 1) + ". ";
-      }
-      desc += "\nUse ``=lookup <name>`` if you can't remember the full name.\n\n"
-      client.channels.find("id", "533085381894078484").send({
-        embed: {
-          color: 7419530,
-          author: {
-            name: "Rare Character!"
-          },
-          description: desc,
-          image: {
-            url: charactersJson[currentCharacter].imageURL
-          }
-        }
-      });
+      title = "Rare Character!";
+      spawnableCharacters = charactersJson.filter((f) => f.rare);
     } else {
       rare = false;
-      var notRareCharacters = charactersJson.filter(f => !f.rare);
-      var ran = Math.floor(Math.random() * notRareCharacters.length);
-
-      currentCharacter = charactersJson.findIndex(f => f.name.toLowerCase() == notRareCharacters[ran].name.toLowerCase());
-      var desc = "A character appeared!\nTry guessing their name with ``=claim <name>`` to claim them!\n\nHints:\nThis character's initials are ";
-      for (var i = 0; i < charactersJson[currentCharacter].name.split(" ").length; i++) {
-        desc += charactersJson[currentCharacter].name.split(" ")[i].substring(0, 1) + ". ";
-      }
-      desc += "\nUse ``=lookup <name>`` if you can't remember the full name.\n\n"
-      client.channels.find("id", "533085381894078484").send({
-        embed: {
-          color: 7419530,
-          author: {
-            name: "Character"
-          },
-          description: desc,
-          image: {
-            url: charactersJson[currentCharacter].imageURL
-          }
-        }
-      });
+      title = "Character";
+      spawnableCharacters = charactersJson.filter((f) => !f.rare);
     }
+
+    currentCharacter = charactersJson.findIndex((f) => f.name.toLowerCase() == spawnableCharacters[Math.floor(Math.random() * spawnableCharacters.length)].name.toLowerCase());
+    var desc = "A character appeared!\nTry guessing their name with ``=claim <name>`` to claim them!\n\nHints:\nThis character's initials are ";
+    for (var i = 0; i < charactersJson[currentCharacter].name.split(" ").length; i++) {
+      desc += charactersJson[currentCharacter].name.split(" ")[i].substring(0, 1) + ". ";
+    }
+    desc += "\nUse ``=lookup <name>`` if you can't remember the full name.\n\n"
+    client.channels.find("id", "533085381894078484").send({
+      embed: {
+        color: 7419530,
+        author: {
+          name: title
+        },
+        description: desc,
+        image: {
+          url: charactersJson[currentCharacter].imageURL
+        }
+      }
+    });
 
     if (boxRandom < 5) {
       currentBoxKeyColor = keysJson[Math.floor(Math.random() * keysJson.length)];
@@ -261,10 +227,10 @@ client.on("message", async msg => {
       //Canvas.registerFont("IndieFlower.ttf", { family: "Indie Flower" });
 
       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      ctx.textAlign = "center"; 
+      ctx.textAlign = "center";
       ctx.font = "72px sans-serif";
-      ctx.fillText(randomString, 445.50, 106.46);
-      
+      ctx.fillText(randomString, canvas.width / 2, 106.46);
+
       //const boxImg = new Discord.Attachment(canvas.toBuffer(), "sockbox.png");
 
       fs.writeFileSync(require.resolve("./currentbox.png"), canvas.toBuffer());
@@ -306,39 +272,7 @@ client.on("message", async msg => {
 });
 
 client.on("guildMemberAdd", member => {
-  var creditsJson = readJson("./credits.json");
-
-  var currentUser = -1;
-
-  for (var i = 0; i < creditsJson.length; i++) {
-    if (creditsJson[i].id == member.user.id) {
-      currentUser = member.user.id;
-    }
-  }
-
-  if (currentUser == -1) {
-    creditsJson.push({
-      "id": member.user.id,
-      "credits": 0,
-      "nextDaily": 0,
-      "characters": [
-
-      ],
-      "wishlist": [
-
-      ],
-      "favoriteCharacter": -1,
-      "lastGacha": 0,
-      "currentTrade": -1,
-      "keys": [
-
-      ],
-      "lastDailyKeys": 0
-    });
-    console.log("Added " + member.user.username);
-  }
-  fs.writeFileSync(require.resolve("./credits.json"), JSON.stringify(creditsJson));
-
+  checkForNewMembers(member);
 });
 
 client.login("NTQwOTEyNjI2NTk5NzIzMDI4.DzX_vw.MxFh0-Qa0srmaDmt1wS6YXvWmeg");
--- commands/characters/leaderboard.js
+++ commands/characters/leaderboard.js
@@ -3,60 +3,6 @@ const {
 } = require("discord.js-commando");
 var fs = require("fs");
 
-module.exports = class LeaderboardCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "leaderboard",
-            group: "characters",
-            memberName: "leaderboard",
-            description: "View the current top 5 rankings.",
-            examples: [
-                "leaderboard"
-            ]
-        });
-    }
-
-    async run(msg) {
-        var creditsJson = readJson("../../credits.json");
-        var charactersJson = readJson("../../characters.json");
-
-        var allUsers = [];
-
-        for (var i = 0; i < creditsJson.length; i++) {
-            var totalOwned = 0;
-            for (var j = 0; j < charactersJson.length; j++) {
-                if (creditsJson[i].characters.filter(f => f.id == j).length > 0) {
-                    totalOwned++;
-                }
-            }
-            allUsers.push({
-                "username": msg.client.users.get(creditsJson[i].id).username,
-                "completion": (parseFloat(totalOwned) / parseFloat(charactersJson.length)) * 100
-            });
-        }
-
-        var allUsersSorted = sort(allUsers);
-
-        var desc = "";
-
-        desc += ":first_place: " + allUsersSorted[allUsersSorted.length - 1].username + " " + allUsersSorted[allUsersSorted.length - 1].completion.toFixed(2) + "%\n\n";
-        desc += ":second_place: " + allUsersSorted[allUsersSorted.length - 2].username + " " + allUsersSorted[allUsersSorted.length - 2].completion.toFixed(2) + "%\n\n";
-        desc += ":third_place: " + allUsersSorted[allUsersSorted.length - 3].username + " " + allUsersSorted[allUsersSorted.length - 3].completion.toFixed(2) + "%\n\n";
-        desc += ":military_medal: " + allUsersSorted[allUsersSorted.length - 4].username + " " + allUsersSorted[allUsersSorted.length - 4].completion.toFixed(2) + "%\n\n";
-        desc += ":reminder_ribbon: " + allUsersSorted[allUsersSorted.length - 5].username + " " + allUsersSorted[allUsersSorted.length - 5].completion.toFixed(2) + "%";
-
-        return msg.say({
-            embed: {
-                color: 7419530,
-                author: {
-                    name: "🏆 Leaderboard 🏆"
-                },
-                description: desc
-            }
-        });
-    }
-}
-
 function sort(originalArray) {
 
     // If array is empty or consists of one element then return this array since it is sorted.
@@ -114,3 +60,57 @@ function mergeSortedArrays(leftArray, rightArray) {
 var readJson = (path) => {
     return JSON.parse(fs.readFileSync(require.resolve(path)));
 }
+
+module.exports = class LeaderboardCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "leaderboard",
+            group: "characters",
+            memberName: "leaderboard",
+            description: "View the current top 5 rankings.",
+            examples: [
+                "leaderboard"
+            ]
+        });
+    }
+
+    async run(msg) {
+        var creditsJson = readJson("../../credits.json");
+        var charactersJson = readJson("../../characters.json");
+
+        var allUsers = [];
+
+        for (var i = 0; i < creditsJson.length; i++) {
+            var totalOwned = 0;
+            for (var j = 0; j < charactersJson.length; j++) {
+                if (creditsJson[i].characters.filter((f) => f.id == j).length > 0) {
+                    totalOwned++;
+                }
+            }
+            allUsers.push({
+                "username": msg.client.users.get(creditsJson[i].id).username,
+                "completion": (parseFloat(totalOwned) / parseFloat(charactersJson.length)) * 100
+            });
+        }
+
+        var allUsersSorted = sort(allUsers);
+
+        var desc = "";
+
+        desc += ":first_place: " + allUsersSorted[allUsersSorted.length - 1].username + " " + allUsersSorted[allUsersSorted.length - 1].completion.toFixed(2) + "%\n\n";
+        desc += ":second_place: " + allUsersSorted[allUsersSorted.length - 2].username + " " + allUsersSorted[allUsersSorted.length - 2].completion.toFixed(2) + "%\n\n";
+        desc += ":third_place: " + allUsersSorted[allUsersSorted.length - 3].username + " " + allUsersSorted[allUsersSorted.length - 3].completion.toFixed(2) + "%\n\n";
+        desc += ":military_medal: " + allUsersSorted[allUsersSorted.length - 4].username + " " + allUsersSorted[allUsersSorted.length - 4].completion.toFixed(2) + "%\n\n";
+        desc += ":reminder_ribbon: " + allUsersSorted[allUsersSorted.length - 5].username + " " + allUsersSorted[allUsersSorted.length - 5].completion.toFixed(2) + "%";
+
+        return msg.say({
+            embed: {
+                color: 7419530,
+                author: {
+                    name: "🏆 Leaderboard 🏆"
+                },
+                description: desc
+            }
+        });
+    }
+};
